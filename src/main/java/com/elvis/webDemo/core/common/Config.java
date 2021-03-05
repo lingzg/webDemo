335d55dcc5cbb4d4798cca394007cc4c4b04d541
lingzenggui <lingzenggui@163.com>
2021-03-05 17:22:42 +0800

    commit

--- src/main/java/com/elvis/webDemo/core/common/Config.java
+++ src/main/java/com/elvis/webDemo/core/common/Config.java
@@ -1,13 +1,9 @@
 package com.elvis.webDemo.core.common;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-@Order(Integer.MIN_VALUE)
 @Component
-@PropertySource(value = "classpath:config.properties")
 public class Config {
     
     @Value("${upload.fileDir}")
